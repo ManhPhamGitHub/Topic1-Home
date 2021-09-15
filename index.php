7ccf3773c748a562155685a827cb09c118135ed8
manh2001827 <66905530+manh2001827@users.noreply.github.com>
2021-09-16 00:19:05 +0700

    add source nhaaaaaa

---
+++ index.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+    <link rel="stylesheet" href="assest/style.css">
+    <link rel="stylesheet" href="assest/responsive.css">
+</head>
+<body>
+    <div class="header">
+        <div class="header-logo">
+            <img src="assest/img/logo.jpg" width="100%" height="100%" />
+        </div>
+        <div class="header-nav">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Categories</a>
+
+                </li>
+                <li>
+                    <a href="#">Foods</a>
+
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="body">
+        <div class="search">
+            <div class="search-Img">
+                <img src="assest/img/blue.jpg" width="100%" height="400px"/>
+            </div>
+            <div class="search-field">
+                <input type="text" placeholder="Search for food ..." />
+                <button>Search</button>
+            </div>
+        </div>
+        <div class="explore">
+            <div class="explore-title">
+                <h2>Explore Foods</h2>
+            </div>
+            <div class="explore-item">
+                <div class="explore-itemCard">
+                    <img src="assest/img/2.jpg" alt="" width="100%" height="100%">
+                    <div class="explore-itemCard-title">
+                        <p>Pizza</p>
+                    </div>
+                </div>
+
+                <div class="explore-itemCard">
+                    <img src="assest/img/3.jpg" alt="" width="100%" height="100%">
+                    <div class="explore-itemCard-title">
+                        <p>Burger</p>
+                    </div>
+                </div>
+
+                <div class="explore-itemCard">
+                    <img src="assest/img/4.jpg" alt="" width="100%" height="100%">
+                    <div class="explore-itemCard-title">
+                        <p>Momos</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="menu">
+            <div class="menu-heading">
+                <h3>
+                    Food Menu
+                </h3>
+            </div>
+            <div class="menu-item">
+                <div class="menu-itemCard">
+                    <div class="meun-itemCard__img">
+                        <img src="assest/img/5.jpg" width="100%" height="100%" />
+                    </div>
+                    <div class="menu-itemCard__details">
+                        <h4>
+                            Food title
+                        </h4>
+                        <h5>19,99$</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, magnam.</p>
+                        <button>Order now</button>
+                    </div>
+                </div>
+
+                <div class="menu-itemCard">
+                    <div class="meun-itemCard__img">
+                        <img src="assest/img/6.jpg" width="100%" height="100%" />
+                    </div>
+                    <div class="menu-itemCard__details">
+                        <h4>
+                            Food title
+                        </h4>
+                        <h5>19,99$</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, magnam.</p>
+                        <button>Order now</button>
+                    </div>
+                </div>
+
+                <div class="menu-itemCard">
+                    <div class="meun-itemCard__img">
+                        <img src="assest/img/7.jpg" width="100%" height="100%" />
+                    </div>
+                    <div class="menu-itemCard__details">
+                        <h4>
+                            Food title
+                        </h4>
+                        <h5>19,99$</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, magnam.</p>
+                        <button>Order now</button>
+                    </div>
+                </div>
+
+                <div class="menu-itemCard">
+                    <div class="meun-itemCard__img">
+                        <img src="assest/img/3.jpg" width="100%" height="100%" />
+                    </div>
+                    <div class="menu-itemCard__details">
+                        <h4>
+                            Food title
+                        </h4>
+                        <h5>19,99$</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, magnam.</p>
+                        <button>Order now</button>
+                    </div>
+                </div>
+
+                <div class="menu-itemCard">
+                    <div class="meun-itemCard__img">
+                        <img src="assest/img/4.jpg" width="100%" height="100%" />
+                    </div>
+                    <div class="menu-itemCard__details">
+                        <h4>
+                            Food title
+                        </h4>
+                        <h5>19,99$</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, magnam.</p>
+                        <button>Order now</button>
+                    </div>
+                </div>
+
+                <div class="menu-itemCard">
+                    <div class="meun-itemCard__img">
+                        <img src="assest/img/5.jpg" width="100%" height="100%" />
+                    </div>
+                    <div class="menu-itemCard__details">
+                        <h4>
+                            Food title
+                        </h4>
+                        <h5>19,99$</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, magnam.</p>
+                        <button>Order now</button>
+                    </div>
+                </div>
+
+                
+            </div>
+            <div class="menu-item__showMore">
+                <a href="#">See All Foods</a>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="socialMedia">
+            <div class="socialMedia-icon">
+                <a href="https://www.facebook.com/manh278" target="_blank"> 
+                    <i class="fab fa-facebook"></i>
+                </a>
+            </div>
+            <div class="socialMedia-icon">
+                <a href="#">
+                    <i class="fab fa-instagram-square"></i>
+
+                </a>
+            </div>
+            <div class="socialMedia-icon">
+                <a href="#">
+                <i class="fab fa-twitter"></i>
+                </a>
+            </div>
+        </div>
+        <div class="author">
+            All right reserved Designed By 
+            <span>
+                <a href="https://github.com/manh2001827" target="_blank">Manh Pham</a>
+            </span>
+        </div>
+    </div>
+</body>
+</html>
